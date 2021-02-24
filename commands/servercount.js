5f40ce3d30e7384d533740df1708519d3608baa9
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2021-02-24 10:22:48 +0530

    Delete commands directory

--- commands/servercount.js
+++
@@ -1,8 +0,0 @@
-const emotes = require("../config/emojis.json");
-
-exports.run = async (client, message) => {
-	message.channel.send({
-		embed: {
-	title: "Server Count",
-	description: (`I'm in **${client.guilds.cache.size}** Server`)}})
-};
